1680b196626ac95aa0e82008c8b83bfca5dc572b
[React2] 박찬호 <qkrcksgh5654@gmail.com>
2025-03-25 16:40:32 +0900

    레디스로 변경

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const { SESSION_NAME, SESSION_SERECT_KEY } = require('./consts/app');
-require('./db_init');
-const apiController = require('./controllers');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const { SESSION_NAME, SESSION_SERECT_KEY } = require("./consts/app");
+require("./db_init");
+const apiController = require("./controllers");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const RedisStore = require("connect-redis").default;
+const Redis = require("ioredis");
 
-require('./services/movie/movie.service');
-require('./services/person/fetchPopularActors.service');
-const MemoryStore = require('memorystore')(session);
+const redisClient = new Redis({
+  host: "redis-14565.c340.ap-northeast-2-1.ec2.redns.redis-cloud.com",
+  port: 14565,
+});
 
 const app = express();
 app.use(express.json());
@@ -17,16 +20,19 @@ app.use(
   session({
     name: SESSION_NAME,
     secret: SESSION_SERECT_KEY,
-    resave: 'false',
-    saveUninitialized: true,
-    store: new MemoryStore({
-      checkPeriod: 24 * 60 * 60 * 1000,
-    }),
+    resave: false,
+    saveUninitialized: false,
+    store: new RedisStore({ client: redisClient }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: "None",
+    },
   })
 );
 
-app.use('images', express.static('images'));
+app.use("/images", express.static("images"));
 app.use(cookieParser());
-app.use('/api', apiController);
+app.use("/api", apiController);
 
 module.exports = app;
