76e8f1c7216c63e0efc26c12d5f5a321c35c36ba
[React2] 박찬호 <qkrcksgh5654@gmail.com>
2025-03-27 16:06:37 +0900

    로그인수정3

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
     origin: FRONT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Custom-Header"],
   })
 );
 
